ce7030844f517c7e8b6f565c6ae230901715f772
amayalw <amayalw@uw.edu>
2022-03-10 03:20:44 -0800

    add chart 3

--- server.R
+++ server.R
@@ -1,8 +1,9 @@
 library(tidyverse)
 library(shiny)
-library(lintr)
-library(styler)
+# library(lintr)
+# library(styler)
 library(plotly)
+library(ggplot2)
 
 #Heres where we load our data.
 
@@ -13,39 +14,39 @@ project_data<-read.csv("https://raw.githubusercontent.com/info-201b-wi22/explora
 # A dodged bar chart showing the high school outcomes for each offender type
 # (justice-involved, not justice-involved, juvenile offender, status offender)
 avg_pct_offenders <- project_data %>%
-  filter(DemographicGroup == "All Students") %>% 
-  filter(DemographicValue == "All Students") %>% 
-  filter(CohortYearTTL > 2015) %>% 
-  rename("Percent_of_Cohort" = "Pct") %>% 
+  filter(DemographicGroup == "All Students") %>%
+  filter(DemographicValue == "All Students") %>%
+  filter(CohortYearTTL > 2015) %>%
+  rename("Percent_of_Cohort" = "Pct") %>%
   rename("Justice_Involvement" = "JJOffenderType")
     # dataset for chart
-    chart1_plot <- avg_pct_offenders %>% 
-      filter(Justice_Involvement %in% input$user_justice_choice)
-    
-    # make plot
-plot <- ggplot(data = chart1_plot) +
-      geom_col(
-        mapping = aes(x = Justice_Involvement, y = Percent_of_Cohort, fill = HSOutcome), position = "dodge"
-      ) + scale_y_continuous(breaks = seq(0, .83, .1)) + facet_wrap(~CohortYearTTL) +
-      labs(title = "High School Outcomes for Each Offender Type",
-           x = "Justice Involvement Type",
-           y = "Percent of Cohort",
-           color = "High School Outcomes")
-    
-    plotly_plot1 <- ggplotly(plot)
-    return(plotly_plot1)
+    # chart1_plot <- avg_pct_offenders %>%
+    #   filter(Justice_Involvement %in% input$user_justice_choice)
+#     
+#     # make plot
+# plot <- ggplot(data = chart1_plot) +
+#       geom_col(
+#         mapping = aes(x = Justice_Involvement, y = Percent_of_Cohort, fill = HSOutcome), position = "dodge"
+#       ) + scale_y_continuous(breaks = seq(0, .83, .1)) + facet_wrap(~CohortYearTTL) +
+#       labs(title = "High School Outcomes for Each Offender Type",
+#            x = "Justice Involvement Type",
+#            y = "Percent of Cohort",
+#            color = "High School Outcomes")
+#     
+#     plotly_plot1 <- ggplotly(plot)
+#     return(plotly_plot1)
   
-  output$outcome <- renderText({
-    if(input$user_explain == 1){
-      return(" If a student does not have a graduation record or a GED record, 
-             they are considered to be a dropout.")
-    } else if (input$user_explain == 2) {
-      return(" If the student does not have a graduation record, 
-             the OSPI P210 and SBCTC Completion records are queried to determine if the student has completed a GED.")
-    } else {
-      return("OSPI P210 indicates the student graduated.")
-    }
-  })
+  # output$outcome <- renderText({
+  #   if(input$user_explain == 1){
+  #     return(" If a student does not have a graduation record or a GED record, 
+  #            they are considered to be a dropout.")
+  #   } else if (input$user_explain == 2) {
+  #     return(" If the student does not have a graduation record, 
+  #            the OSPI P210 and SBCTC Completion records are queried to determine if the student has completed a GED.")
+  #   } else {
+  #     return("OSPI P210 indicates the student graduated.")
+  #   }
+  # })
 
 
 # Chart 2 
@@ -54,40 +55,40 @@ plot <- ggplot(data = chart1_plot) +
 
 # Make chart 2.
 
-ggplotly(chart2)
-return(chart2)
+# ggplotly(chart2)
+# return(chart2)
 
 #Chart 3
 
 # Load chart 3 data.
 
-categories<-c("American Indian or Alaska Native","Asian","Black or African American","Multiple Races (Details Unknown)","Native Hawaiian and Other Pacific Islander","Spanish/Hispanic/Latino","White")
-
-pct_offenders <-project_data%>%
-  filter(DemographicValue%in%categories)
-
-pct_offenders <-pct_offenders%>%
-  filter(DemographicValue%in%input$user_category)
-
-# Find  a way to pick by year.
-
-pct_offenders <-pct_offenders%>%
-  filter(CohortYearTTL%in%input$user_category2)
-
-pct_offenders <-pct_offenders%>%
-  filter(JJOffenderType%in%input$user_category3)
-
-# Make chart 3.
-
-chart3 <- ggplot(data = pct_offenders) +
-  geom_col(mapping = aes(x = HSOutcome, 
-                         y = Pct, 
-                         fill = DemographicValue), position="dodge")+
-  labs(title = "Outcomes per Racial Group", x = "Outcome", y = "Percentage")
-
-# Make interactive plot
-my_plotly_plot <- ggplotly(chart3) 
-return(my_plotly_plot)
+# categories<-c("American Indian or Alaska Native","Asian","Black or African American","Multiple Races (Details Unknown)","Native Hawaiian and Other Pacific Islander","Spanish/Hispanic/Latino","White")
+# 
+# pct_offenders <-project_data%>%
+#   filter(DemographicValue%in%categories)
+# 
+# pct_offenders <-pct_offenders%>%
+#   filter(DemographicValue%in%input$user_category)
+# 
+# # Find  a way to pick by year.
+# 
+# pct_offenders <-pct_offenders%>%
+#   filter(CohortYearTTL%in%input$user_category2)
+# 
+# pct_offenders <-pct_offenders%>%
+#   filter(JJOffenderType%in%input$user_category3)
+# 
+# # Make chart 3.
+# 
+# chart3 <- ggplot(data = pct_offenders) +
+#   geom_col(mapping = aes(x = HSOutcome, 
+#                          y = Pct, 
+#                          fill = DemographicValue), position="dodge")+
+#   labs(title = "Outcomes per Racial Group", x = "Outcome", y = "Percentage")
+# 
+# # Make interactive plot
+# my_plotly_plot <- ggplotly(chart3) 
+# return(my_plotly_plot)
 
 #Server function!
 
--- ui.R
+++ ui.R
@@ -2,9 +2,10 @@ library(ggplot2)
 library(plotly)
 library(bslib)
 
-library(bslib)
+library(shiny)
 #install.packages("htmlwidgets", type = "binary")
 #install.packages("DT", type = "binary")
+project_data<-read.csv("https://raw.githubusercontent.com/info-201b-wi22/exploratory-analysis-sara-mustredr09/main/Juvenile_Justice_Dashboard_-_HS_Completion.csv?token=GHSAT0AAAAAABQIEWSA6BKJCPJ3VN3C5VHSYQYGLCA")
 
 intro_tab <- tabPanel(
   "Introduction",
@@ -17,125 +18,79 @@ intro_tab <- tabPanel(
 
 # small widgets
 
-plot1_tab <- tabPanel(
-  "Educational Outcome and Justice Involvement",
-  sidebarLayout(
-    sidebarPanel(
-      selectInput(
-    inputId = "user_justice_choice",
-    label = h3("Select an Offender Type"),
-    choices = avg_pct_offenders$Justice_Involvement,
-    selected = "Justice Involved",
-    multiple = TRUE),
-  
-  radioButtons(
-    inputId = "user_explain",
-    label = h5("See the Metric explanation of High School Outcomes (HSOutcome):"),
-    choices = list("Dropout" = 1, "GED" = 2, "HS Diploma" = 3),
-    selected = NULL),
-  textOutput(outputId = "outcome")
-  
-  )
- ),
-
-mainPanel(
-  plotlyOutput(outputId = "chart1"),
-  p("We wanted to demonstrate the changes over time for the high school outcomes for different types of justice involvement.",
-    "These graphs show the percent for each high school outcome (GED, diploma, and dropout) for each type of justice involvement within different years.",
-    "It is pretty obvious that the most optismistic high school outcome shows on those students who are not justice involved: ",
-    "highest rate of HS Diploma and lowest rate of dropout."),
- )
-)
-
-# layer of the tab
-chart1_tab <- tabPanel(
-  "Educational Outcome and Justice Involvement",
-  sidebarLayout(
-    plot_sidebar,
-    plot_main
-  ),
-  fluidPage(theme = bs_theme(bootswatch = "sketchy"),
-            p("We wanted to demonstrate the changes over time for the high school outcomes for different types of justice involvement.",
-              "These graphs show the percent for each high school outcome (GED, diploma, and dropout) for each type of justice involvement within different years.",
-              "It is pretty obvious that the most optismistic high school outcome shows on those students who are not justice involved: ",
-              "highest rate of HS Diploma and lowest rate of dropout.")
-  )
-)
-
-#Chart 2 Page
-
-plot2_tab <- tabPanel(
-  "Chart 2",
-  
-  #This is the sidebar.
-  sidebarLayout(
-    sidebarPanel(
-      p("Select your viewing options!"),
-      selectInput("county_id",
-                  label = h3("County"),
-                  choices = fires$Counties,
-                  selected = "option"
-      ),
-      selectInput("color_id2",
-                  label = h3("Color"),
-                  choices = brewer.pal(8, "Set2")
-      )
-    ),
-    
-    #Main panel where the chart + description are supposed to go.
-    mainPanel(
-      h2("Title that describes the chart"),
-      p("Description"),
-      plotlyOutput(outputId = "output id for ch2"),
-      p("...")
-    )
-  )
-)
-
-p("We wanted to demonstrate the changes over time for the high school outcomes for different types of justice involvement.",
-  "These graphs show the percent for each high school outcome (GED, diploma, and dropout) for each type of justice involvement within different years.",
-  "It is pretty obvious that the most optismistic high school outcome shows on those students who are not justice involved: ",
-  "highest rate of HS Diploma and lowest rate of dropout.")
-
-# Chart 3.
-
+# plot1_tab <- tabPanel(
+#   "Educational Outcome and Justice Involvement",
+#   fluidPage(theme = bs_theme(bootswatch = "sketchy"),
+#   sidebarLayout(
+#     sidebarPanel(
+#       selectInput(
+#         inputId = "user_justice_choice",
+#         label = h3("Select an Offender Type"),
+#         choices = avg_pct_offenders$Justice_Involvement,
+#         selected = "Justice Involved",
+#         multiple = TRUE),
+# 
+#       radioButtons(
+#         inputId = "user_explain",
+#         label = h5("See the Metric explanation of High School Outcomes (HSOutcome):"),
+#         choices = list("Dropout" = 1, "GED" = 2, "HS Diploma" = 3),
+#         selected = 1),
+#       textOutput(outputId = "outcome")
+#     ),
+#     mainPanel(
+#       plotlyOutput(outputId = "chart1"),
+#       p("We wanted to demonstrate the changes over time for the high school outcomes for different types of justice involvement.",
+#         "These graphs show the percent for each high school outcome (GED, diploma, and dropout) for each type of justice involvement within different years.",
+#         "It is pretty obvious that the most optismistic high school outcome shows on those students who are not justice involved: ",
+#         "highest rate of HS Diploma and lowest rate of dropout.")
+#     ),
+#     p("We wanted to demonstrate the changes over time for the high school outcomes for different types of justice involvement.",
+#                 "These graphs show the percent for each high school outcome (GED, diploma, and dropout) for each type of justice involvement within different years.",
+#                 "It is pretty obvious that the most optismistic high school outcome shows on those students who are not justice involved: ",
+#                 "highest rate of HS Diploma and lowest rate of dropout.")
+# 
+#   )
+# ))
+#
+# # Chart 3.
+#
 plot3_tab <- tabPanel(
   "Chart 3",
   sidebarLayout(
-    sidebarPanel( 
-selectInput(
+    sidebarPanel(
+      selectInput(
         inputId = "user_category",
         label = "Select Demographic Group",
         choices = pct_offenders$DemographicValue,
         selected = "Asian",
         multiple = TRUE),
-      
+
       selectInput(
         inputId = "user_category2",
         label = "Select Year",
         choices = pct_offenders$CohortYearTTL,
         selected = "2013",
         multiple = TRUE),
-      
+
       selectInput("user_category3",
                   label = h3("Offender Type"),
                   choices = pct_offenders$JJOffenderType,
                   selected = "Status Offender"
       )
-    )
-      ),
-    
-    # Main panel where the chart + description are supposed to go.
-mainPanel(
+    ),
+    mainPanel(
       h2("High School outcomes per race"),
       plotlyOutput(outputId = "chart3"),
-      p("This visualization demonstrates high school outcomes for different racial groups each year. We chose this chart to reveal how race and involvement with the criminal justice system might 
-  affect high school outcomes. The fact that there is little to no data on certain races for offender status 
-  rates might reveal how racial steoreotypes influence both high school outcomes and involvement with the criminal justice system.
-"),
-      
+      p("This visualization demonstrates high school outcomes for different racial groups each year. We chose this chart to reveal how race and involvement with the criminal justice system might
+        affect high school outcomes. The fact that there is little to no data on certain races for offender status
+        rates might reveal how racial steoreotypes influence both high school outcomes and involvement with the criminal justice system.
+      ")
     )
+
+    # Main panel where the chart + description are supposed to go.
+
   )
+)
 
 # Conclusion tab.
 
@@ -148,8 +103,8 @@ conclusion_tab <- tabPanel(
 ui <- navbarPage(
   "General page title",
   intro_tab,
-  plot1_tab,
-  plot2_tab,
+  # plot1_tab,
+  # plot2_tab,
   plot3_tab,
   conclusion_tab
 )
